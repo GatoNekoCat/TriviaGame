e61aaa3f8311fd60383d46b76442a111c364ee78
Raiicon <orankai@gmail.com>
2018-11-15 22:45:23 -0500

    game is playable now. Will go through all the questions it has and then show you a game over screen with your score attached. Going to add a timer counting down tonight and then save styling for tomorrow

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,7 @@ triviaGame = {
     highScore: 0,
     triviaQuestions: [{}],
     score: 0,
+    answer: 0,
     
 
 
@@ -18,20 +19,30 @@ triviaGame = {
              q1 = {
                  q:"what alias does Jeffery Lebowski use?",
                  pa:["Dude","Walter","Marmot","Jackie Treehorn"],
-                 an: 0
+                 an: "0"
              },
              q2 = {
                  q:"Who peed on the dudes rug?",
                  pa:["Nihilists","Fascists","Woo","Jackie Treehorn"],
-                 an: 2
+                 an: "2"
              },
+             q3 = {
+                 q:"Which actor played The Stranger?",
+                 pa:["Harrison Ford","Sam Davis","Sam Elliot","Nicholas Cage"],
+                 an: "2"
+             },
+             q4 = {
+                 q:"What date is written on the check the dude writes to pay for his milk?",
+                 pa:["Nov 5th","Sept 11th","Oct 31st","April 20th"],
+                 an: "1"
+             }
         
 
         ];
         // Create a button to trigger the start of the game
         var bttn = $("<button>");
         $(bttn).attr('id', 'start-button');
-        bttn.text("Click to start the game!")
+        bttn.text("Click to start the game!");
         $("#play-field").append(bttn);
     },
     // This function chooses a random question from the array in triviaGame.triviaQuestions
@@ -39,9 +50,38 @@ triviaGame = {
     // displayed as buttons 
     chooseRandomQuestion: function() {
 
+            // Empty the playfield first
+            $("#play-field").empty(); 
+
+        // an initial check to see if there is a question to get. If not, game is over.
+        if (triviaGame.triviaQuestions === undefined || triviaGame.triviaQuestions.length == 0) {
+            // stop the interval
+            clearInterval(guessTime);
+            // array empty or does not exist
+            var gameOver = $("<h1>");
+            gameOver.text("Game Over");
+            gameOver.attr('id', "game-over");
+            $("#play-field").append(gameOver);
+            var scoreText = $("<h2>");
+            scoreText.text("Score: " +triviaGame.score);
+            $("#play-field").append(scoreText);
+            var playAgain = $("<button>");
+            playAgain.text("Play again?");
+            playAgain.attr("id", "play-again-btn");
+            $("#play-field").append(playAgain);
+            $("#play-again-btn").on('click', function(){
+                triviaGame.init();
+
+            })
+
+        };
+
         // generates a random question object from the array of question objects. 
-        question = triviaGame.triviaQuestions[Math.floor(Math.random() * triviaGame.triviaQuestions.length)];
-        
+        var question = triviaGame.triviaQuestions[Math.floor(Math.random() * triviaGame.triviaQuestions.length)];
+        // next I want the index location of the questioon
+        var x = triviaGame.triviaQuestions.indexOf(question);
+        // then I remove the question from the list of questions, keeping the game from going on forever over the same questions.
+        triviaGame.triviaQuestions.splice(x, 1);
         // create a header to display the question
         var questionHeader = $("<h1>");
         $(questionHeader).attr('id', "askQuestion");
@@ -61,11 +101,26 @@ triviaGame = {
             $("#play-field").append(a);
 
         }
+        // store the answer number in the object
+       triviaGame.answer = question.an;
+    //  listen for the button click
+    $(".question-button").on('click',function(){
+        console.log(this.id);
+        console.log(triviaGame.answer);
+        // checks button id against the answer. If the right button is chosen the score is increased. If not, nothing happens and
+        // it will call chooseRandomQuestion either way.
+        if (this.id === triviaGame.answer){
+            triviaGame.score++;
+            triviaGame.chooseRandomQuestion();
+        };
+        triviaGame.chooseRandomQuestion();
+        });
         
     }
 
-    
+// end of triviaGame 
 };
+var guessTime;
 
 $( document ).ready(function() {
 
@@ -73,16 +128,16 @@ $( document ).ready(function() {
 triviaGame.init();
 $("#start-button").on('click', function() {
         // Empty the play field, and call the chooseRandomQuestion to display the questions
-        $("#play-field").empty(); 
        triviaGame.chooseRandomQuestion();
-        if (triviaGame.triviaQuestions >= 1){
-            setInterval(chooseRandomQuestion, 30000);
+    //    This is where the time for each question goes ***
+    clearInterval(guessTime);
+    guessTime = setInterval(triviaGame.chooseRandomQuestion, 3000);
 
-        } 
+        });
+   
+        
+ });
 
 
-
-});
-});
 
 
